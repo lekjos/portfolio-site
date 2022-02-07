1ae12a8d907d484e9954b767717f4b87ffd78d69
lekjos <leif@leifkjos.com>
2022-02-06 18:18:12 -0800

    add tests for new views

--- PortfolioSite/Main/tests/test_views.py
+++ PortfolioSite/Main/tests/test_views.py
@@ -8,7 +8,24 @@ class BasePermissionsTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.c = Client()
-    
+
+class HomePageTest(BasePermissionsTest):
     def test_home(self):
         r = self.c.get('/')
         self.assertTrue(r.status_code==200, msg='Home page should return 200 not logged in')
+
+class AboutTest(BasePermissionsTest):
+    """
+    Test About View
+    """
+    def test_about(self):
+        r = self.c.get('/about/')
+        self.assertTrue(r.status_code==200, msg='About should return 200 not logged in')
+
+class ContactTest(BasePermissionsTest):
+    """
+    Test Contact View
+    """
+    def test_home(self):
+        r = self.c.get('/contact/')
+        self.assertTrue(r.status_code==200, msg='Contact should return 200 not logged in')
