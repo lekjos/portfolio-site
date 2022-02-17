b8e1c242946d2373f23fd387d34e533859b36e18
lekjos <leif@leifkjos.com>
2022-02-16 16:59:10 -0800

    remove views

--- PortfolioSite/Main/tests/test_views.py
+++ PortfolioSite/Main/tests/test_views.py
@@ -15,18 +15,18 @@ class HomePageTest(BaseTest):
         self.assertTrue(r.status_code==200, msg='Home page should return 200 not logged in')  
 
 
-class ContactTest(BaseTest):
-    """
-    Test Contact View
-    """
-    def test_home(self):
-        r = self.c.get('/contact/')
-        self.assertTrue(r.status_code==200, msg='Contact should return 200 not logged in')
+# class ContactTest(BaseTest):
+#     """
+#     Test Contact View
+#     """
+#     def test_home(self):
+#         r = self.c.get('/contact/')
+#         self.assertTrue(r.status_code==200, msg='Contact should return 200 not logged in')
     
-    def test_title(self):
-        r = self.c.get('/contact/')
-        self.assertContains(r,'<title>Contact')
+#     def test_title(self):
+#         r = self.c.get('/contact/')
+#         self.assertContains(r,'<title>Contact')
 
-    def test_form_in_context(self):
-        r = self.c.get('/contact/')
+#     def test_form_in_context(self):
+#         r = self.c.get('/contact/')
         
--- PortfolioSite/Main/views.py
+++ PortfolioSite/Main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
+from Main.models import Project
 
 # Create your views here.
 class Home(TemplateView):
@@ -8,13 +9,10 @@ class Home(TemplateView):
     Site Home Page
     """
     template_name='index.html'
-class About(TemplateView):
-    """
-    Site Home Page
-    """
-    template_name='about.html'
-class Contact(TemplateView):
-    """
-    Site Home Page
-    """
-    template_name='contact.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['projects'] = Project.objects.all().values('title','images__image')
+
+        return context
+
