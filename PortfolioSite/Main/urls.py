509807444a57b2259f1eb362b2f3493aa6381ada
lekjos <leif@leifkjos.com>
2022-02-16 16:51:33 -0800

    remove urls

--- PortfolioSite/Main/urls.py
+++ PortfolioSite/Main/urls.py
@@ -3,6 +3,4 @@ from Main import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='index'),
-    path('about/', views.About.as_view(), name='about'),
-    path('contact/', views.Contact.as_view(), name='contact'),
     ]
