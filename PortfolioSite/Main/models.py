f2d9651574f1a6eef1d81e636e674d07e38f0f00
lekjos <leif@leifkjos.com>
2022-02-21 12:44:54 -0800

    add re-ordering for images and projects

--- PortfolioSite/Main/models.py
+++ PortfolioSite/Main/models.py
@@ -1,9 +1,10 @@
 from ipaddress import ip_address
-from django.db import models
+from django.db import models, transaction
+from django.db.models import F, Max
 from tinymce import models as tinymce_models 
 from django.conf import settings
 from django.urls import reverse
-
+from django.apps import apps
 # Create your models here.
 
 class Email(models.Model):
@@ -38,10 +39,93 @@ class Email(models.Model):
         subj = (self.subject[:75] + '...') if len(self.subject) > 75 else self.subject
         return " - ".join([self.name, subj])
 
+# From https://www.revsys.com/tidbits/keeping-django-model-objects-ordered/
+class StepManager(models.Manager):
+    """
+    Manager to order models
+    """
+
+    def move(self, obj, new_order):
+        """ Move an object to a new order position """
+
+        qs = self.get_queryset()
+
+        with transaction.atomic():
+            if hasattr(obj, 'step_related_to_model'):
+
+                # qs = eval(f"self.filter({step_related_to_model})")
+                related_model = apps.get_model(obj.step_related_to_app,obj.step_related_to_model)
+                kwgs = {obj.step_related_to_field: obj.pk}
+                related_object = related_model.objects.get(**kwgs)
+                qs = self.filter(project__pk=related_object.id)
+            else:
+                qs = self.all()
+
+            if obj.order > int(new_order):
+                qs.filter(
+                    order__lt=obj.order,
+                    order__gte=new_order,
+                ).exclude(
+                    pk=obj.pk
+                ).update(
+                    order=F('order') + 1,
+                )
+            else:
+                qs.filter(
+                    order__lte=new_order,
+                    order__gt=obj.order,
+                ).exclude(
+                    pk=obj.pk,
+                ).update(
+                    order=F('order') - 1,
+                )
+
+            obj.order = new_order
+            obj.save()
+    
+    def create(self, related_pk=None, **kwargs):
+        """ Create a new object in last position"""
+        instance = self.model(**kwargs)
+        
+        with transaction.atomic():
+            # Get our current max order number
+            if hasattr(instance, 'step_related_to_model'):
+                if not related_pk:
+                    raise ValueError("Related PK required")
+                qs = eval(f"self.filter({instance.step_related_to_model.lower()}__pk={int(related_pk)})")
+            else:
+                qs = self.all()
+            results = qs.aggregate(
+                Max('order')
+            )
+
+            # Increment and use it for our new object
+            current_order = results['order__max']
+            if current_order is None:
+                current_order = 0
+
+            value = current_order + 1
+            instance.order = value
+            instance.save()
+
+            if hasattr(instance, 'step_related_to_model'):
+                if not related_pk:
+                    raise ValueError("Related PK required")
+                related_model = apps.get_model(app_label=instance.step_related_to_app, model_name=instance.step_related_to_model)
+                related_obj = related_model.objects.get(pk=related_pk)
+                eval(f"related_obj.{instance.step_related_to_field}.add({instance.id})")
+                
+
+            return instance
+
 class Image(models.Model):
     """
     Stores images for projects
     """
+    step_related_to_app = 'Main'
+    step_related_to_model = 'Project'
+    step_related_to_field = 'images'
+    
     title = models.CharField(max_length=200)
     image = models.ImageField(
         upload_to='images/'
@@ -49,6 +133,13 @@ class Image(models.Model):
     order = models.PositiveSmallIntegerField(
         null=True
     )
+    caption = tinymce_models.HTMLField(
+        max_length=1500,
+        null=True,
+        blank=True,
+    )
+
+    objects = StepManager()
 
     def get_absolute_url(self):
         return f"{settings.MEDIA_URL}{self.image.url}"
@@ -63,8 +154,14 @@ class Project(models.Model):
     title = models.CharField(max_length=500)
     description = tinymce_models.HTMLField()
     images = models.ManyToManyField(Image)
+    order = models.PositiveSmallIntegerField(
+        unique=True
+    )
+
+    objects = StepManager()
 
     def get_absolute_url(self):
         return reverse('project', kwargs={'pk':self.pk})
+    
     def __str__(self):
         return str(self.title)
--- PortfolioSite/Main/tests/test_models.py
+++ PortfolioSite/Main/tests/test_models.py
@@ -1,6 +1,11 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.conf import settings
+import os
 from ipaddress import ip_address
 from Main.tests.test_views import BaseTest
 from Main.models import Email, Project, Image
+from pprint import pprint
+import shutil
 
 class EmailTest(BaseTest):
     """
@@ -51,4 +56,122 @@ class ImageTest(BaseTest):
         testme = Image(title='titlesdfsd')
         stringrep_expected = str(testme.title)
         self.assertEqual(str(testme), stringrep_expected, "String_rep should be same as second arg.")
+
+FIXTURES_DIR = os.path.join(settings.BASE_DIR,'fixtures/test_img.jpg')
+
+class StepManagerTest(BaseTest):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_proj1 = Project.objects.create(
+            title="test title 1",
+            description='<p>test description </p>',
+        )
+        cls.test_proj2 = Project.objects.create(
+            title="test title 2",
+            description='<p>test description </p>',
+        )
+
+        cls.proj1_img1 = Image.objects.create(
+            title="proj1 img1",
+            image = SimpleUploadedFile(name='test_image.jpg', content=open(FIXTURES_DIR, 'rb').read(), content_type='image/jpeg'),
+            caption='<p>test description </p>',
+            related_pk=cls.test_proj1.id
+        )
+        cls.proj1_img2 = Image.objects.create(
+            title="proj1 img2",
+            image = SimpleUploadedFile(name='test_image.jpg', content=open(FIXTURES_DIR, 'rb').read(), content_type='image/jpeg'),
+            caption='<p>test description </p>',
+            related_pk=cls.test_proj1.id
+        )
+
+        cls.proj2_img1 = Image.objects.create(
+            title="proj2 img1",
+            image = SimpleUploadedFile(name='test_image.jpg', content=open(FIXTURES_DIR, 'rb').read(), content_type='image/jpeg'),
+            caption='<p>test description </p>',
+            related_pk=cls.test_proj2.id
+        )
+        cls.proj2_img2 = Image.objects.create(
+            title="proj2 img2",
+            image = SimpleUploadedFile(name='test_image.jpg', content=open(FIXTURES_DIR, 'rb').read(), content_type='image/jpeg'),
+            caption='<p>test description </p>',
+            related_pk=cls.test_proj2.id
+        )
+
+    
+    def tearDown(self):
+        Project.objects.all().delete()
+        Image.objects.all().delete()
+
+        #delete media files
+        dir_path = settings.MEDIA_ROOT
+        if os.path.exists(dir_path):
+            shutil.rmtree(dir_path)
+
+
+    def test_create_project(self):
+        """
+        Test Create Method
+        """
+        self.assertEqual(self.test_proj1.order,1)
+        self.assertEqual(self.test_proj2.order,2)
+    
+    def test_img_uploaded(self):
+        """
+        verify that image uploaded successfully
+        """
+        img = Image.objects.get(title="proj1 img1", project=self.test_proj1)
+        self.assertEquals(img.title, self.proj1_img1.title)
+        self.assertEquals(img.order, 1)
+
+    def test_move_project(self):
+        """
+        Test moving object to new position
+        """
+        self.assertEqual(self.test_proj1.order,1, "initial value")
+        self.assertEqual(self.test_proj2.order,2, "initial value")
+        
+        Project.objects.move(self.test_proj1, 2)
+
+        self.test_proj1 = Project.objects.get(title="test title 1")
+        self.test_proj2 = Project.objects.get(title="test title 2")
+        self.assertEqual(self.test_proj1.order,2, "Updated value")
+        self.assertEqual(self.test_proj2.order,1, "Updated value")
+
+        Project.objects.move(self.test_proj1, 1)
+        
+        self.test_proj1 = Project.objects.get(title="test title 1")
+        self.test_proj2 = Project.objects.get(title="test title 2")
+        self.assertEqual(self.test_proj1.order,1, "Back to initial")
+        self.assertEqual(self.test_proj2.order,2, "Back to initial")
+
+    
+    def test_move_img(self):
+        """
+        test moving images
+        """
+        self.assertEqual(self.proj1_img1.order,1)
+        self.assertEqual(self.proj1_img2.order,2)
+        self.assertEqual(self.proj2_img1.order,1)
+        self.assertEqual(self.proj2_img2.order,2)
+
+        Image.objects.move(self.proj1_img1, 2)
+        self.proj1_img1 = Image.objects.get(title="proj1 img1")
+        self.proj1_img2 = Image.objects.get(title="proj1 img2")
+        self.assertEqual(self.proj1_img1.order,2, 'order changed')
+        self.assertEqual(self.proj1_img2.order,1, 'order changed')
+        self.assertEqual(self.proj2_img1.order,1, 'order unchanged')
+        self.assertEqual(self.proj2_img2.order,2, 'order unchanged')
+
+        Image.objects.move(self.proj1_img1, 1)
+        self.proj1_img1 = Image.objects.get(title="proj1 img1")
+        self.proj1_img2 = Image.objects.get(title="proj1 img2")
+        self.assertEqual(self.proj1_img1.order,1, 'order returned to initial')
+        self.assertEqual(self.proj1_img2.order,2, 'order returned to initial')
+        self.assertEqual(self.proj2_img1.order,1, 'order unchanged')
+        self.assertEqual(self.proj2_img2.order,2, 'order unchanged')
+
+
+
+
         
