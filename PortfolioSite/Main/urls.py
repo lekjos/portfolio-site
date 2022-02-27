ec530f8088a5344b7761ab116748f02cb5c77ff4
lekjos <leif@leifkjos.com>
2022-02-26 23:22:54 -0800

    add contact form

--- PortfolioSite/Main/urls.py
+++ PortfolioSite/Main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('projects/<int:pk>/', views.ProjectDetail.as_view(), name='project'),
     path('ajax/move-project/<int:pk>/', views.MoveProject.as_view(), name='move_project'),
     path('ajax/move-image/<int:pk>/', views.MoveImage.as_view(), name='move_image'),
+    path('ajax/contact/', views.Contact.as_view(), name='contact'),
     ]
