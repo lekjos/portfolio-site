dbc22dff21980f3d59cd02dd93faa30c7a309de3
lekjos <leif@leifkjos.com>
2022-02-26 23:22:58 -0800

    continued

--- PortfolioSite/Main/views.py
+++ PortfolioSite/Main/views.py
@@ -1,13 +1,19 @@
+from smtplib import SMTPException
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.db.models import Subquery, OuterRef
-from django.db.models import Max, F
+from django.core.mail import send_mail
+from django.db.models import Subquery, OuterRef, Max, F
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound, JsonResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, DetailView
-from Main.models import Project, Image
-from django.conf import settings
-import json
+
+from Main.models import Project, Image, Email
+from Main.helpers import get_client_ip
+
+from pprint import pprint
+import json, logging
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 class Home(TemplateView):
@@ -40,6 +46,7 @@ class ProjectDetail(DetailView):
 
         return context
 
+    
 #ajax views
 mimetype='application/json'
 
@@ -95,6 +102,43 @@ class MoveProject(AjaxMoveBaseView):
 class MoveImage(AjaxMoveBaseView):
     model = Image
 
+class Contact(View):
+    def post(self, request, **kwargs):
+        try:
+            print('body', request.POST)
 
-            
+        except json.JSONDecodeError:
+            # print('bad data found')
+            return JsonResponse({'status': 'bad request'}, status=400)
+        
+        ip = get_client_ip(request)
+
+        e = Email(
+            name=request.POST.get('sender_name'),
+            email=request.POST.get('email'),
+            body=request.POST.get('message'),
+            subject=request.POST.get('subject'),
+            ip_address=ip,
+        )
+        try:
+            body = f"""leifkjos.com contact form email:
+Name: {e.name}
+Email: {e.email}
+IP: {e.ip_address}
+
+Subject: {e.subject}
+
+Message:
+{e.body}
+
+"""
+            status = send_mail(f"Message From {e.email}: {e.subject}",body,settings.EMAIL_CONTACT_FROM_ADDRESS,[settings.EMAIL_CONTACT_TO_ADDRESS])
+        except Exception as e:
+            logger.exception(f'Failed sending contact form email from {e.name} {e.email}, subject: {e.subject}', e.args)
+            status = 0
+        
+        e.status = status
+        e.save()
+
+        return JsonResponse('OK',safe=False,status=200)
             
--- PortfolioSite/static/vendor/php-email-form/validate.js
+++ PortfolioSite/static/vendor/php-email-form/validate.js
@@ -3,6 +3,7 @@
 * URL: https://bootstrapmade.com/php-email-form/
 * Author: BootstrapMade.com
 */
+
 (function () {
   "use strict";
 
@@ -15,37 +16,26 @@
       let thisForm = this;
 
       let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
-        return;
-      }
+      // if( ! action ) {
+      //   displayError(thisForm, 'The form action property is not set!')
+      //   return;
+      // }
       thisForm.querySelector('.loading').classList.add('d-block');
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
 
       let formData = new FormData( thisForm );
+      let captcha = document.forms['contactForm']['captcha'].value.trim().toLowerCase();
+      let error = '';
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error)
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
+      if (parseInt(captcha) == 4 || captcha == 'four'){
         php_email_form_submit(thisForm, action, formData);
+      } else {
+        error = '2+2 does not equal '+ captcha;
+        displayError(thisForm, error);
       }
+
     });
   });
 
@@ -64,7 +54,7 @@
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
+      if (String(data.trim()) == String('"OK"')) {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
