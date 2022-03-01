d454774d9e4a83d9c24a611818c38c4ebddcecb9
lekjos <leif@leifkjos.com>
2022-02-28 18:09:29 -0800

    redo next previous

--- PortfolioSite/Main/helpers.py
+++ PortfolioSite/Main/helpers.py
@@ -1,5 +1,8 @@
 
 # from https://stackoverflow.com/questions/4581789/how-do-i-get-user-ip-address-in-django
+from django.urls import reverse
+
+
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
@@ -9,15 +12,41 @@ def get_client_ip(request):
     return ip
 
 
-def in_dictlist(key, value, my_dictlist, default=False, ignore_key_error=False):
+def find_next_and_previous(id_:int, qs:dict, view_name='project') -> dict:
     """
-    Checks for key value pair in a list of dictionaries. Returns tuple (index, matching dict) or default (false if no kwarg).
+    Returns dict of next and previous item's ID and Title. Pass in ordered queryset as dictionary with 'id' and 'title' fields.
+    Optional arg view_name: used for reverse()
+
+    Output:
+    {
+        'previous_id': <int>,
+        'previous_title': <str>,
+        'next_id': <int>,
+        'next_title': <str>,
+    }
     """
-    for i, entry in enumerate(my_dictlist):
-        try:
-            if entry[key] == value:
-                return i, entry
-        except KeyError:
-            if ignore_key_error:
-                pass
-    return default
+    match_found=False
+    r = {
+        'previous_id': None,
+        'previous_url': None,
+        'previous_title': None,
+        'next_id': None,
+        'next_url': None,
+        'next_title': None,
+    }
+
+    for entry in qs:
+        if match_found:
+            r['next_id'] = entry['id']
+            r['next_url'] = reverse(view_name, kwargs={'pk':entry['id']})
+            r['next_title'] = entry['title']
+            return r
+        if entry['id'] == id_:
+            match_found=True
+        else:
+            r['previous_id']=entry['id']
+            r['previous_url'] = reverse(view_name, kwargs={'pk':entry['id']})
+            r['previous_title']=entry['title']
+        
+
+    return r
