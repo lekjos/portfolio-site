87bd2696f3329cbf1453b2bc8391b4fa7d66d376
lekjos <leif@leifkjos.com>
2022-03-07 10:26:33 -0800

    update urls

--- PortfolioSite/Main/urls.py
+++ PortfolioSite/Main/urls.py
@@ -3,7 +3,7 @@ from Main import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='index'),
-    path('projects/<int:pk>/', views.ProjectDetail.as_view(), name='project'),
+    path('projects/<slug:slug>/', views.ProjectDetail.as_view(), name='project'),
     path('ajax/move-project/<int:pk>/', views.MoveProject.as_view(), name='move_project'),
     path('ajax/move-image/<int:pk>/', views.MoveImage.as_view(), name='move_image'),
     path('ajax/contact/', views.Contact.as_view(), name='contact'),
