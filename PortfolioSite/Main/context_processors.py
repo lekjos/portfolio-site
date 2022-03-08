641cd4177573c89a9fbe67a27e28048dc846338f
lekjos <leif@leifkjos.com>
2022-03-07 17:55:12 -0800

    update context processor

--- PortfolioSite/Main/context_processors.py
+++ PortfolioSite/Main/context_processors.py
@@ -4,7 +4,7 @@ def global_processor(request):
     """
     Adds globally available context for base template.
     """
-    root_url = settings.ROOT_URL.replace('http://',"").replace('https://',"").replace('nojessery','NoJessery')
+    root_url = settings.ROOT_URL
 
     return {
         "root_url": root_url,
