fef53ba4094524385351f52f6ac1dde96e8a2092
lekjos <leif@leifkjos.com>
2022-05-11 16:40:41 -0600

    split internal ips by space in env

--- PortfolioSite/PortfolioSite/settings.py
+++ PortfolioSite/PortfolioSite/settings.py
@@ -97,7 +97,7 @@ FORCE_DEBUG_TOOLBAR = True if os.getenv('FORCE_DEBUG_TOOLBAR', 'False') == 'True
 if DEBUG or FORCE_DEBUG_TOOLBAR:
     INSTALLED_APPS.append("debug_toolbar")
     MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
-    INTERNAL_IPS = list(os.getenv("INTERNAL_IPS", "127.0.0.1").split())
+    INTERNAL_IPS = list(os.getenv("INTERNAL_IPS", "127.0.0.1").split(" "))
     
 
 WSGI_APPLICATION = os.getenv('WSGI_APPLICATION','PortfolioSite.wsgi.application')
