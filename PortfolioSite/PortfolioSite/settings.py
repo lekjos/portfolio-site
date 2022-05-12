43a537daed17bbb5068f85c0096ef82d72d60f96
lekjos <leif@leifkjos.com>
2022-05-12 12:18:28 -0600

    change default permission and auth

--- PortfolioSite/PortfolioSite/settings.py
+++ PortfolioSite/PortfolioSite/settings.py
@@ -295,7 +295,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # "rest_framework.authentication.BasicAuthentication",
-        # "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ]
 }
