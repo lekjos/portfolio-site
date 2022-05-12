25300372545924bf4782db4be718aa26d232665d
lekjos <leif@leifkjos.com>
2022-05-11 18:13:39 -0600

    add robots.txt

--- PortfolioSite/Main/urls.py
+++ PortfolioSite/Main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from Main import views
+from django.views.generic.base import TemplateView #import TemplateView
+
 
 urlpatterns = [
     path('', views.Home.as_view(), name='index'),
@@ -7,4 +9,6 @@ urlpatterns = [
     path('ajax/move-project/<int:pk>/', views.MoveProject.as_view(), name='move_project'),
     path('ajax/move-image/<int:pk>/', views.MoveImage.as_view(), name='move_image'),
     path('ajax/contact/', views.Contact.as_view(), name='contact'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt',content_type="text/plain")),
+
     ]
