8fdb87f1a0f0db901540dfd9b4fd93a88901f74b
lekjos <leif@leifkjos.com>
2022-05-12 09:30:22 -0600

    add custom author permission

---
+++ PortfolioSite/Blog/api/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+class AuthorModifyOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+    def has_object_permission(self, request, view, obj):
+        """
+        Anonymous users can read, author and superuser can read/write.
+        """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return (request.user == obj.author) or request.user.is_superuser
